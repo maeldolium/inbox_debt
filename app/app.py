03ff8e03da3a553284a2022f4f616ee87ffdde86
Maël <119709626+maeldolium@users.noreply.github.com>
2026-02-11 23:14:15 +0100

    feat: persist safelist domains using JSON storage and CLI management

--- app/app.py
+++ app/app.py
@@ -2,6 +2,7 @@ from auth.oauth_flow import auth
 from gmail_api.fetch_emails import get_gmail_service, list_unsubscribe_emails
 from gmail_api.actions import trash_message
 from config.config import SAFE_DOMAINS
+from config.safelist_manager import load_safelist, save_safelist, add_domain_to_safelist
 import webbrowser
 
 def main():
@@ -19,41 +20,77 @@ def main():
         print("Aucun domaine à traiter")
         return
     
-    filtered_senders = {}
+    # Chargement de la safelist
+    safelist = load_safelist()
 
     # Filtrer les domaines
+    filtered_senders = {}
+
     for domain in dict_senders:
-        if domain not in SAFE_DOMAINS:
+        if domain not in safelist:
             filtered_senders[domain] = dict_senders[domain]
 
     # Lister les domaines avec le nombre de mails
     mapping = {i + 1: {'domain': domain, 'count': filtered_senders[domain]['count']} 
            for i, domain in enumerate(filtered_senders)}
 
+    # Tant qu'il y a des domaines dans mapping
+    # Afficher le menu
+    while mapping:
+        # Choix du domaine par l'utilisateur
+        print("Quel domaine voulez-vous traiter ?")
 
-    # Choix du domaine par l'utilisateur
-    print("Quel domaine voulez-vous supprimer ?")
+        for id, data in mapping.items():
+            print(f'{id}. {data["domain"]} ({data["count"]} mails)')
 
-    for id, data in mapping.items():
-        print(f'{id}. {data["domain"]} ({data["count"]} mails)')
-
-    while True:
+        # Choix de l'utilisateur
         try:
-            user_choice = int(input("Votre choix: "))
+            domain_id = int(input("Votre choix: "))
             # Si choix valide
-            if user_choice in mapping:
-                domain = mapping[user_choice]['domain']
-                count = mapping[user_choice]['count']
+            if domain_id in mapping:
+                domain = mapping[domain_id]['domain']
+                count = mapping[domain_id]['count']
 
-                user_choice = str(input(f'Êtes-vous sûr de vouloir supprimer {domain} ? ({count} mails) (y/n)'))
-                # Validation du choix
-                if user_choice == 'y':
-                    trash_message(service, filtered_senders[domain]["message_ids"])
-                    return
+                # Action à réaliser
+                print("Que voulez vous faire ?\n 1. Supprimer\n 2. Ajouter à la safelist\n 3. Retour")
+
+                action = int(input("Votre choix: "))
+
+                match(action):
+                    # Suppression
+                    case 1:
+                        confirmation = str(input(f'Êtes-vous sûr de vouloir supprimer {domain} ? ({count} mails) (y/n)'))
+                
+                        # Validation du choix
+                        if confirmation == 'y':
+                            trash_message(service, filtered_senders[domain]["message_ids"])
+                            del filtered_senders[domain]
+                            mapping = {i + 1: {'domain': d, 'count': filtered_senders[d]['count']} 
+                                      for i, d in enumerate(filtered_senders)}
+                    #  Ajout à la safelist
+                    case 2:
+                        add_domain_to_safelist(domain)
+                        del filtered_senders[domain]
+                        mapping = {i + 1: {'domain': d, 'count': filtered_senders[d]['count']} 
+                                  for i, d in enumerate(filtered_senders)}
+                    #  Retour
+                    case 3:
+                        continue
+                    #  Choix invalide
+                    case _:
+                        print("Choix invalide")
+                    
             # Sinon
             else:
                 print("Choix invalide !")
+        # Choix ne correspond pas
         except ValueError:
-            print("Entrez un numéro !")    
+            print("Entrez un numéro !")
+                    
+
+
+                
+
+
     
 main()
---
+++ app/config/safelist_manager.py
@@ -0,0 +1,39 @@
+import os.path
+import json
+
+def load_safelist():
+    if os.path.exists("app/config/safelist.json"):
+        with open("app/config/safelist.json", "r") as file:
+            data = json.load(file)
+            
+    else:
+        with open("app/config/safelist.json", "x") as file:
+            json.dump([], file, indent=4)
+        with open("app/config/safelist.json", "r") as file:
+            data = json.load(file)
+
+    return data
+
+def save_safelist(safelist):
+    if os.path.exists("app/config/safelist.json"):
+        with open("app/config/safelist.json", "w") as file:
+            json.dump(safelist, file, indent=4)
+    else:
+        print("Fichier introuvable")
+
+    
+def add_domain_to_safelist(domain):
+    safelist = load_safelist()
+    print(domain)
+    domain = domain.lower().strip()
+
+    if domain not in safelist:
+        safelist.append(domain)
+        save_safelist(safelist)
+        print("Domaine ajouté à la liste.")
+        return True
+    else:
+        print("Domaine déjà dans la liste.")
+        return False
+    
+
