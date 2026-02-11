0220ab31ef5dde32ab5454d4603f3be37422bf11
Maël <maelandre2@gmail.com>
2026-02-11 20:49:45 +0100

    feat: add safelist to prevent deletion of important domains

--- app/app.py
+++ app/app.py
@@ -1,6 +1,7 @@
 from auth.oauth_flow import auth
 from gmail_api.fetch_emails import get_gmail_service, list_unsubscribe_emails
 from gmail_api.actions import trash_message
+from config.config import SAFE_DOMAINS
 import webbrowser
 
 def main():
@@ -13,38 +14,43 @@ def main():
     # Récupération des mails
     dict_senders = list_unsubscribe_emails(service)
     
+    # Si pas de domaine
     if not dict_senders:
         print("Aucun domaine à traiter")
         return
-
-    # Afficher les domaines et leur nombre d'occurrences
-    print(f"{'Domain':<30} {'Count':<8} {'Subjects':<10} {'Links':<10}")
-    print("-" * 60)
-    for domain, data in dict_senders.items():
-        print(f"{domain:<30} {data['count']:<8} {len(data['subjects']):<10} {len(data['unsubscribe_links']):<10}")
     
-    # Lister les domaines
-    mapping = {i + 1: domain for i, domain in enumerate(dict_senders)}
+    filtered_senders = {}
 
+    # Filtrer les domaines
+    for domain in dict_senders:
+        if domain not in SAFE_DOMAINS:
+            filtered_senders[domain] = dict_senders[domain]
+
+    # Lister les domaines avec le nombre de mails
+    mapping = {i + 1: {'domain': domain, 'count': filtered_senders[domain]['count']} 
+           for i, domain in enumerate(filtered_senders)}
+
+
+    # Choix du domaine par l'utilisateur
     print("Quel domaine voulez-vous supprimer ?")
 
-    for id, domain in mapping.items():
-        print(f'{id}. {domain}')
+    for id, data in mapping.items():
+        print(f'{id}. {data["domain"]} ({data["count"]} mails)')
 
-    # Choix du domaine par l'utilisateur et suppresion des mails
-    # correspondant si l'utilisateur confirme son choix
     while True:
         try:
             user_choice = int(input("Votre choix: "))
-
+            # Si choix valide
             if user_choice in mapping:
-                domain = mapping[user_choice]
+                domain = mapping[user_choice]['domain']
+                count = mapping[user_choice]['count']
 
-                user_choice = str(input(f'Etes vous sûr de vouloir supprimer {domain} ? (y/n)'))
-
+                user_choice = str(input(f'Êtes-vous sûr de vouloir supprimer {domain} ? ({count} mails) (y/n)'))
+                # Validation du choix
                 if user_choice == 'y':
-                    trash_message(service, dict_senders[domain]["message_ids"])
+                    trash_message(service, filtered_senders[domain]["message_ids"])
                     return
+            # Sinon
             else:
                 print("Choix invalide !")
         except ValueError:
