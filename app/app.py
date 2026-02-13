09af8a31481eb3fe382624789d1af77c009e1cf6
Maël <maelandre2@gmail.com>
2026-02-13 21:00:09 +0100

    feat: add number of mails with ou without link in deletion summary before confirmation

--- app/app.py
+++ app/app.py
@@ -1,7 +1,7 @@
 from auth.oauth_flow import auth
 from gmail_api.fetch_emails import get_gmail_service, list_unsubscribe_emails
 from gmail_api.actions import trash_message
-from ux.ux import display_domains, select_domain, display_actions, select_action, confirm_deletion
+from ux.ux import display_domains, select_domain, display_actions, select_action, confirm_deletion, count_with_without_link_mails
 from config.safelist_manager import load_safelist, save_safelist, add_domain_to_safelist, filter_safelist
 import webbrowser
 
@@ -27,7 +27,7 @@ def main():
     filtered_senders = filter_safelist(dict_senders, safelist)
 
     # Lister les domaines avec le nombre de mails
-    mapping = {i + 1: {'domain': domain, 'count': filtered_senders[domain]['count'], 'subjects': filtered_senders[domain]['subjects']} 
+    mapping = {i + 1: {'domain': domain, 'count': filtered_senders[domain]['count'], 'subjects': filtered_senders[domain]['subjects'], 'unsubscribe_links': filtered_senders[domain]['unsubscribe_links']} 
            for i, domain in enumerate(filtered_senders)}
 
     # Tant qu'il y a des domaines dans mapping
@@ -50,7 +50,7 @@ def main():
             case 0: break
             # Supprimer les mails
             case 1:
-                if confirm_deletion(domain, count, subjects) == True:
+                if confirm_deletion(domain, count, subjects, filtered_senders[domain]['unsubscribe_links']) == True:
                     trash_message(service, filtered_senders[domain]["message_ids"])
                     del filtered_senders[domain]
                     mapping = {i + 1: {'domain': d, 'count': filtered_senders[d]['count']} 
